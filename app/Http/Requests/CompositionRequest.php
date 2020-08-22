3e5f835b9951a9334ea56fd5dff39585a8d6c638
Xenx <xenx.efy@gmail.com>
2020-08-22 21:53:50 +0300

    Валидация запроса на поиска треков.

--- app/Http/Requests/CompositionRequest.php
+++ app/Http/Requests/CompositionRequest.php
@@ -7,8 +7,10 @@ use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Validation\Rule;
 
-class GetTracks extends FormRequest
+class CompositionRequest extends FormRequest
 {
+
+    protected $redirect = false;
     /**
      * Determine if the user is authorized to make this request.
      *
@@ -34,7 +36,9 @@ class GetTracks extends FormRequest
             'sortType' => [
                 Rule::in(['asc', 'desc']),
                 'required_with:sortBy'
-            ]
+            ],
+            'tags.*' => 'numeric',
+            'title' => 'nullable|min:3',
         ];
     }
 
@@ -44,12 +48,14 @@ class GetTracks extends FormRequest
             'sortBy.in' => 'Сортировка может быть по title или по last_played полям.',
             'sortType.in' => 'Тип соритровки может быть asc или desc.',
             'sortType.required_with' => 'sortBy должен быть заполнен.',
-            'sortBy.required_with' => 'sortType должен быть заполнен.'
+            'sortBy.required_with' => 'sortType должен быть заполнен.',
+            'tags.*.numeric' => 'Необходимо чтобы теги были числами.',
+            'title.min' => 'Название произведения не должно быть меньше трех символов.',
         ];
     }
 
     protected function failedValidation(Validator $validator)
     {
-        throw new HttpResponseException(response()->json($validator->errors(), 400));
+        throw new HttpResponseException(response()->json(['status' => 'error', 'errors' => $validator->errors()], 200));
     }
 }
